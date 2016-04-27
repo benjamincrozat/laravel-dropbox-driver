0375f8fc085b5cdaefc3d7fbaf6dfcc89d15defb
Benjamin Crozat <benjamin@croz.at>
2016-04-27 15:50:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Laravel Dropbox Driver
+
+Dropbox driver for Laravel.
+
+## Usage
+
+First, install it:
+```php
+composer require benjamincrozat/laravel-dropbox-driver
+```
+
+In your ```config/app.php```, add the service provider:
+```php
+'providers' => [
+
+    BC\LaravelDropboxDriver\ServiceProvider::class,
+
+],
+```
+
+Next, add the following in ```app/filesystems.php```:
+```php
+'disks' => [
+
+    'dropbox' => [
+        'driver' => 'dropbox',
+        'secret' => env('DROPBOX_SECRET'),
+        'token' => env('DROPBOX_TOKEN'),
+    ],
+
+],
+```
+
+Then, in your ```.env``` file:
+```
+DROPBOX_SECRET=your_app_secret_key
+DROPBOX_TOKEN=your_access_token
+```
+
+## License
+
+[WTFPL](http://www.wtfpl.net/about/)
---
+++ src/ServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BC\LaravelDropboxDriver;
+
+use Storage;
+use League\Flysystem\Filesystem;
+use Dropbox\Client as DropboxClient;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use League\Flysystem\Dropbox\DropboxAdapter;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('dropbox', function ($app, $config) {
+            $client = new DropboxClient($config['token'], $config['app_secret']);
+
+            return new Filesystem(new DropboxAdapter($client));
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
