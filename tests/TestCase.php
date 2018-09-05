ff61f5e609ec3d8931b4b99695c710093492c713
Benjamin Crozat <benjamincrozat@me.com>
2018-09-05 11:47:12 +0200

    Removed unnecessary stuff

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,13 +6,7 @@ use Illuminate\Foundation\Application;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
+    protected function getEnvironmentSetUp(Application $app)
     {
         if (file_exists(__DIR__ . '/../.env')) {
             (new \Dotenv\Dotenv(__DIR__ . '/..'))->load();
@@ -20,20 +14,14 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
         $app['config']->set('filesystems.disks.dropbox', [
             'driver' => 'dropbox',
-            'token' => env('DROPBOX_TOKEN'),
+            'token'  => env('DROPBOX_TOKEN'),
         ]);
     }
 
-    /**
-     * Get package providers.
-     *
-     * @param  \Illuminate\Foundation\Application $app
-     * @return array
-     */
-    protected function getPackageProviders($app)
+    protected function getPackageProviders(Application $app)
     {
         return [
-            'BC\Laravel\DropboxDriver\ServiceProvider',
+            \BC\Laravel\DropboxDriver\ServiceProvider::class,
         ];
     }
 }
