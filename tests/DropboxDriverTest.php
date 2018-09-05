6bb7d5e77053c642e615764cb799b8ce47fdf6e9
Benjamin Crozat <benjamin.crozat@lagardere-active.com>
2018-09-05 12:11:25 +0200

    Changed way of testing

--- tests/DropboxDriverTest.php
+++ tests/DropboxDriverTest.php
@@ -5,8 +5,13 @@ use Illuminate\Support\Facades\Storage;
 class DropboxDriverTest extends TestCase
 {
     /** @test */
-    function it_creates_and_removes_a_folder()
+    public function it_works()
     {
-        $this->assertNotNull(Storage::disk('dropbox')->directories('/'));
+        // Let's pretend a Dropbox token has been set up.
+        config(['filesystems.disks.dropbox.token' => '']);
+
+        // Just check that the disk is resolved. No need to ping
+        // Dropbox, since spatie/flysystem-dropbox does it.
+        $this->assertInstanceOf(Illuminate\Filesystem\FilesystemAdapter::class, Storage::disk('dropbox'));
     }
 }
